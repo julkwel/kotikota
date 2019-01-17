79dbe1132143d17b6fdc93592469c419b5bfbf78
chrys-elrak <chrysrakk@gmail.com>
2019-01-17 12:52:48 +0300

    New component

---
+++ src/app/kotikota/team-form/team-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-form',
+  templateUrl: './team-form.component.html',
+  styleUrls: ['./team-form.component.css']
+})
+export class TeamFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
