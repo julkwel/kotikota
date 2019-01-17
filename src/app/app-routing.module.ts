96489e92376e466ef59507cf9723f097ea022e39
chrys-elrak <chrysrakk@gmail.com>
2019-01-17 22:08:57 +0300

    Adding new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,28 @@
-import { NgModule }              from '@angular/core';
-import { RouterModule, Routes }  from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import {KotikotaLoginComponent} from "./kotikota-login/kotikota-login.component";
-import {AppComponent} from './app.component';
-import {KotikotaFrontComponent} from "./kotikota-front/kotikota-front.component";
-import {KotikotaDashboardComponent} from "./kotikota-dashboard/kotikota-dashboard.component";
+import { KotikotaLoginComponent } from "./kotikota-login/kotikota-login.component";
+import { AppComponent } from './app.component';
+import { KotikotaFrontComponent } from "./kotikota-front/kotikota-front.component";
+import { KotikotaDashboardComponent } from "./kotikota-dashboard/kotikota-dashboard.component";
+import { TeamFormComponent } from './kotikota/team-form/team-form.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: KotikotaLoginComponent },
   { path: 'dashboard', component: KotikotaDashboardComponent },
-  { path: '',component:KotikotaFrontComponent },
+  { path: 'team-build', component: TeamFormComponent },
+  { path: '', component: KotikotaFrontComponent }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(
-        appRoutes,
-        { enableTracing: true }
+      appRoutes,
+      { enableTracing: true }
     )
   ],
   exports: [
     RouterModule
   ]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
