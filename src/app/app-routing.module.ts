4dfb76a753c10727476e4f35ab579fde3e9b4e0c
chrys-elrak <chrysrakk@gmail.com>
2019-01-17 22:36:57 +0300

    Adding new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { KotikotaLoginComponent } from "./kotikota-login/kotikota-login.component";
-import { AppComponent } from './app.component';
 import { KotikotaFrontComponent } from "./kotikota-front/kotikota-front.component";
 import { KotikotaDashboardComponent } from "./kotikota-dashboard/kotikota-dashboard.component";
 import { TeamFormComponent } from './kotikota/team-form/team-form.component';
+import { GiveupFormComponent } from './kotikota/giveup-form/giveup-form.component';
 
 const appRoutes: Routes = [
-  { path: 'login', component: KotikotaLoginComponent },
-  { path: 'dashboard', component: KotikotaDashboardComponent },
-  { path: 'team-build', component: TeamFormComponent },
-  { path: '', component: KotikotaFrontComponent }
+  { path: "login", component: KotikotaLoginComponent },
+  { path: "dashboard", component: KotikotaDashboardComponent },
+  { path: "team-build", component: TeamFormComponent },
+  { path: "giveup", component: GiveupFormComponent },
+  { path: "", component: KotikotaFrontComponent }
 ];
 
 @NgModule({
