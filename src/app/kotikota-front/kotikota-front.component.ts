71b1afea23435a275453e01d1cf98a0b294e5a92
chrys-elrak <chrysrakk@gmail.com>
2019-01-17 22:22:43 +0300

    Adding dynamic route
    Adding new key and value

--- src/app/kotikota-front/kotikota-front.component.ts
+++ src/app/kotikota-front/kotikota-front.component.ts
@@ -16,21 +16,24 @@ export class KotikotaFrontComponent implements OnInit {
             desc: 'Hanangana ekipa hanao kotikota',
             icon: 'fa fa-group fa-3x light-blue-text',
             color: 'info',
-            button: 'Hanangana'
+            button: 'Hanangana',
+            route: 'team-build'
         },
         {
             title: 'Mila vonjy',
             desc: 'Mila vonjy fa tena tapaka',
             icon: 'fa fa-money fa-3x yellow-text',
             color: 'warning',
-            button: 'Hanangana'
+            button: 'Hanangana',
+            route: 'giveup'
         },
         {
             title: 'Misy marary',
             desc: 'Mila vonjy fa misy marary / fahoriana',
             icon: 'fa fa-heart-o fa-3x red-text',
             color: 'danger',
-            button: 'Hanome'
+            button: 'Hanome',
+            route: 'donate'
         }
     ];
 
