2830a81fc6a592eaab8e81612ce3d7be61ba81fb
chrys-elrak <chrysrakk@gmail.com>
2019-01-18 16:10:06 +0300

    creating footer component

---
+++ src/app/kotikota-footer/kotikota-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kotikota-footer',
+  templateUrl: './kotikota-footer.component.html',
+  styleUrls: ['./kotikota-footer.component.css']
+})
+export class KotikotaFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
